6e0b8bdd09e1e8451eb430f7af11e5072a3be62c
lasiuchenko <yaroslav.lasjuchenko@gmail.com>
2021-07-22 21:55:59 +0000

    corrected have-question part, added js file and used js for  accordion

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// accordion
+const accordion = document.querySelectorAll('.have-question__list__item');
+for (item of accordion) {
+  item.addEventListener('click', function () {
+    if (this.classList.contains('active')) {
+      this.classList.remove('active');
+    } else {
+        for (element of accordion) {
+          element.classList.remove('active');
+      }
+      this.classList.toggle('active');
+    }
+  })
+}
