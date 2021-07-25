153f06df264610de613378dd4ff9dbb208815446
lasiuchenko <yaroslav.lasjuchenko@gmail.com>
2021-07-25 09:58:45 +0000

    added file caruosel.js + added some styles to testimonials part + added and changed new class for common titte+subtittle+text+text-separator

---
+++ js/carousel-testimonials.js
@@ -0,0 +1,46 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slides = document.querySelectorAll(".testimonials__carousel-item");
+const dots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove("carousel-active")
+    }
+    slides[n].classList.add("carousel-active");
+}
+
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove("dot-active")
+    }
+    dots[n].classList.add("dot-active");
+}
+
+const nextSlide = () => {
+    if (currentSlide == slides.length - 1) {
+        currentSlide = 0;
+        activeSlide(currentSlide);
+        activeDot(currentSlide);
+    } else {
+        currentSlide++;
+        activeSlide(currentSlide);
+        activeDot(currentSlide);
+    }
+}
+const prevSlide = () => {
+    if (currentSlide == 0) {
+        currentSlide = slides.length - 1;
+        activeSlide(currentSlide);
+        activeDot(currentSlide);
+    } else {
+        currentSlide--;
+        activeSlide(currentSlide);
+        activeDot(currentSlide);
+    }
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
