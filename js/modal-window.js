181194b68c1db4e23208e636ee60b9636a13336f
lasiuchenko <86733367+lasiuchenko@users.noreply.github.com>
2021-07-28 17:05:52 +0200

    Update modal-window.js
    added function to setTimeout

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,6 +1,6 @@
-let delay_popup = 10000;
+let delay_popup = 4000;
 
-setTimeout(document.querySelector('.modal-window').style.display = "block", delay_popup);
+setTimeout(() => document.querySelector('.modal-window').style.display = "block", delay_popup);
 
 document.querySelector("#yes").addEventListener("click", function () {
     document.querySelector('.modal-window').style.display = "none";
