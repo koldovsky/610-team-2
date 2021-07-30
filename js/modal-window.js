0ed4beb35170839892c12797d7859f09e899f32e
lasiuchenko <86733367+lasiuchenko@users.noreply.github.com>
2021-07-30 16:46:00 +0200

    Update modal-window.js
    fixed modal window

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,5 +1,3 @@
-document.querySelector('.modal-window').style.display = "block", delay_popup;
-
 document.querySelector("#yes").addEventListener("click", function () {
     document.querySelector('.modal-window').style.display = "none";
 })
